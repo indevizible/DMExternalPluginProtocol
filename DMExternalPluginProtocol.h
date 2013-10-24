cf2fdf78987fba7300c7343e19c5f38cef70fe9e
indevizible <wut@2bsimple.com>
2013-10-24 19:19:49 +0700

    initial commit

---
+++ DMExternalPluginProtocol.h
@@ -0,0 +1,19 @@
+//
+//  DMExternalPluginProtocol.h
+//  DMConnex
+//
+//  Created by Trash on 10/24/13.
+//  Copyright (c) 2013 infostant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DMExternalPluginProtocol <NSObject>
+
+@end
+
+@protocol DMExternalHostProtocol <NSObject>
+@optional
+-(id)landingFordata:(NSDictionary *)__data whenPressOrder:(void(^)(void))__whenPressOrder;
+@property (nonatomic,readonly) NSString *oauth;
+@end;
